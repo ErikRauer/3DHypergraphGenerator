85c584bf333b7d56d593186f52417d912209e300
Erik Rauer <eriktr22@gmail.com>
2021-09-01 21:32:40 -0500

    Add a `generateColumn()` method to `HypergraphGenerator`
    Adds a `generateColumn()` method to the `HypergraphGenerator` class, which randomly creates a column for the matrix to use. This column represents an arc in the hypergraph.
    
    Also adds some comments to the `HypergraphGenerator` class.

--- app/src/main/java/DHypergraphGenerator/HypergraphGenerator.java
+++ app/src/main/java/DHypergraphGenerator/HypergraphGenerator.java
@@ -6,6 +6,11 @@ import org.ejml.simple.SimpleMatrix;
 
 public class HypergraphGenerator {
 
+    // (Approximate) Proportion of arcs that should have two heads and two tails respectively
+    // NOTE: number of hyperarcs = TWO_HEAD_PROP + TWO_TAIL_PROP
+    // TODO: Make these parameters in the generate() method
+    final static double TWO_HEAD_PROP = 1/3;
+    final static double TWO_TAIL_PROP = 1/3;
 
     /**
      * Class for the generation of hypergraph vertex-hyperarc incidence matrix
@@ -22,6 +27,7 @@ public class HypergraphGenerator {
 
         ArrayList<SimpleMatrix> matrixList = new ArrayList<SimpleMatrix>();
 
+        // Create numGraphs matrices and add them to matrixList
         for(int i = 0; i < numGraphs; i++) {
 
             SimpleMatrix currentMatrix = createMatrix(numVertices, numArcs);
@@ -32,10 +38,38 @@ public class HypergraphGenerator {
         return(matrixList);
     }
 
+    /**
+     * Randomly generate a hyperarc-incidence matrix
+     * @param numRows number of rows = number of vertices
+     * @param numCols number of columns = number of arcs
+     * @return the hyperarc-incidence matrix as a SimpleMatrix 
+     */
     private SimpleMatrix createMatrix(int numRows, int numCols) {
+
         SimpleMatrix endMatrix = new SimpleMatrix(numRows, numCols);
         return endMatrix;
+
     }
 
+    /**
+     * Generates a column/arc for the hyperarc-incidence matrix
+     * @param numElements the number of elements the column should have = number of vertices in the hypergraph
+     * @return the column/arc as a float[numElements]
+     */
+    private float[] generateColumn(int numElements) {
+        float[] column = new float[numElements];
+
+        // Decide which type of arc/hyperarc to create
+        double columnTypeSeed = Math.random();
+        if(columnTypeSeed < TWO_HEAD_PROP) {
+            // Generate a two headed hyperarc
+        } else if (columnTypeSeed < TWO_HEAD_PROP + TWO_TAIL_PROP) {
+            // Generate a two tailed hyperarc
+        } else {
+            // Generate a regular arc
+        }
+
+        return column;
+    }
     
 }
